9a94a1ad6e8458ab4a6cb1a9aa74ded91f6f3993
カーン ムハンマド ヌール イー アラム <muhammad.noor.e.alam.khan.jk@fujifilm.com>
2026-02-14 00:08:53 +0900

    ..

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask on AWS via GitHub Actions!"
+    return "Hello from Flask on AWS via GitHub Actions! 1"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
